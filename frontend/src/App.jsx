0fc76886e95b432af4ef4c807e36236964528802
Luiz Gustavo Baixo <luizavai@hotmail.com>
2025-05-27 18:35:02 -0300

    chore: checkpoint - React com Tailwind configurado e funcionando

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-blue-500">
+      Tailwind funcionando!
+    </div>
+  )
+}
+
+
+export default App
